bbae04a409bf851d702c978c906ab9a6e075278b
Novish Khalil <novishkhalil2@hotmail.com>
2024-11-11 00:25:37 +0500

    Add files via upload

---
+++ Q16.ts
@@ -0,0 +1,34 @@
+//16.More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+
+myList.push("Maria");
+myList.push("Umaima");
+myList.push("Hamza");
+console.log(myList);
+
+//• Add one new guest to the beginning of your array.
+
+myList.unshift("Waqas");
+console.log(myList);
+
+
+
+//• Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
+
+let meseg: string = "now i found a bigger dinner table at Avari Hotel";
+console.log(`${myList[0]} hey,${meseg}`);
+console.log(`${myList[1]} hey,${meseg}`);
+console.log(`${myList[2]} hey,${meseg}`);
+console.log(`${myList[3]} hey,${meseg}`);
+console.log(`${myList[4]} hey,${meseg}`);
+console.log(`${myList[5]} hey,${meseg}`);
+console.log(`${myList[6]} hey,${meseg}`);
+console.log(`${myList[7]} hey,${meseg}`);
+console.log(`${myList[8]} hey,${meseg}`);
+console.log(`${myList[9]} hey,${meseg}`);
+
+
+
+
+
+
+
